c363725918085f08010de7006b01ba3faba46b77
tihmstar <tihmstar@gmail.com>
2016-06-15 10:30:23 +0200

    Initial Commit

---
+++ img4tool/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  img4tool
+//
+//  Created by tihmstar on 15.06.16.
+//  Copyright © 2016 tihmstar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
