7a2898a68d9744098f4d1a4e825bc40726763e47
tihmstar <tihmstar@gmail.com>
2016-06-15 11:14:59 +0200

    .

--- img4tool/main.c
+++ img4tool/main.c
@@ -7,25 +7,58 @@
 //
 
 #include <stdio.h>
+#include <getopt.h>
+#include <string.h>
 #include <stdlib.h>
-#include "img4.h"
+
+//TODO: Parse arguments
+static struct option longopts[] = {
+    { "help",           no_argument,       NULL, 'h' },
+    { "extract",        required_argument,  NULL, 'e' },
+    { NULL, 0, NULL, 0 }
+};
+
+void cmd_help(){
+    printf("Usage: img4tool [OPTIONS] FILE\n");
+    printf("Parses img4 files\n\n");
+    printf("Last argument must be an img4 file\n");
+    
+    printf("  -h, --help                prints usage information\n");
+    printf("  -e, --extract FILEPATH    extracts data to file\n");
+    printf("\n");
+}
+
 
 int main(int argc, const char * argv[]) {
     
+    int optindex = 0;
+    int opt = 0;
     
-    FILE *f = fopen("iBEC.n66.RELEASE.im4p", "r");
-    fseek(f, 0, SEEK_END);
-    size_t size = ftell(f);
-    fseek(f, 0, SEEK_SET);
+    char *img4FilePath = 0;
+    char *extractedFilePath = 0;
     
-    char * buf = malloc(size);
-    fread(buf, size, 1, f);
+    if (argc == 1){
+        cmd_help();
+        return -1;
+    }
     
-    char *str;
-    char *b2 = ans1GetString(buf+5, &str);
+    while ((opt = getopt_long(argc, (char* const *)argv, "e:h", longopts, &optindex)) > 0) {
+        switch (opt) {
+            case 'h': // long option: "help"; can be called as short option
+                cmd_help();
+                return 0;
+            case 'e': // long option: "extract"; can be called as short option
+                extractedFilePath = optarg;
+                break;
+            default:
+                 cmd_help();
+                 return -1;
+        }
+    }
     
-    free(buf);
-    fclose(f);
+    img4FilePath = malloc(strlen(argv[argc-1])+1);
+    strcpy(img4FilePath, argv[argc-1]);
+    
     
     return 0;
 }
