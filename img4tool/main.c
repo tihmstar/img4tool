5fa565c02cdf81847587e671ab60f17adf52faef
Malte Poll <malte.poll@web.de>
2016-06-15 11:10:32 +0200

    Implemented cli option parsing and help message

--- img4tool/main.c
+++ img4tool/main.c
@@ -7,9 +7,58 @@
 //
 
 #include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdlib.h>
+
+//TODO: Parse arguments
+static struct option longopts[] = {
+    { "help",           no_argument,       NULL, 'h' },
+    { "extract",        required_argument,  NULL, 'e' },
+    { NULL, 0, NULL, 0 }
+};
+
+void cmd_help(){
+    printf("Usage: img4tool [OPTIONS] FILE\n");
+    printf("Parses img4 files\n\n");
+    printf("Last argument must be an img4 file\n");
+    
+    printf("  -h, --help                prints usage information\n");
+    printf("  -e, --extract FILEPATH    extracts encrypted data to file\n");
+    printf("\n");
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int optindex = 0;
+    int opt = 0;
+    
+    char *img4FilePath = 0;
+    char *extractedFilePath = 0;
+    
+    if (argc == 1){
+        cmd_help();
+        return -1;
+    }
+    
+    while ((opt = getopt_long(argc, (char* const *)argv, "e:h", longopts, &optindex)) > 0) {
+        switch (opt) {
+            case 'h': // long option: "help"; can be called as short option
+                cmd_help();
+                return 0;
+            case 'e': // long option: "extract"; can be called as short option
+                extractedFilePath = optarg;
+                break;
+            default:
+                 cmd_help();
+                 return -1;
+        }
+    }
+    
+    img4FilePath = malloc(strlen(argv[argc-1])+1);
+    strcpy(img4FilePath, argv[argc-1]);
+    
+    
     return 0;
 }
