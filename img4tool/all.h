877efce38298bfe08cfad050204b58a09d4fcd6c
tihmstar <tihmstar@gmail.com>
2016-06-15 11:06:28 +0200

    getstring

---
+++ img4tool/all.h
@@ -0,0 +1,14 @@
+//
+//  all.h
+//  img4tool
+//
+//  Created by tihmstar on 15.06.16.
+//  Copyright © 2016 tihmstar. All rights reserved.
+//
+
+#ifndef all_h
+#define all_h
+
+#define error(a ...) printf(a)
+
+#endif /* all_h */
