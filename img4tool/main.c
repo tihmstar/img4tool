ae9cabf008334c0763fced2d1c05cf75394568c1
tihmstar <tihmstar@gmail.com>
2016-07-05 14:24:44 +0200

    .

--- img4tool/main.c
+++ img4tool/main.c
@@ -13,32 +13,35 @@
 #include <plist/plist.h>
 #include "img4.h"
 
-void asd(char *shshfile){
-    FILE *ofp = fopen(shshfile,"rb");
-    fseek(ofp, SEEK_END, 0);
-    size_t shshsize = ftell(ofp);
-    fseek(ofp, SEEK_SET, 0);
-    char *opl = malloc(shshsize);
-    fread(opl, shshsize, 1, ofp);
-    fclose(ofp);
+char *im4mFormShshFile(char *shshfile){
+    FILE *f = fopen(shshfile,"rb");
+    fseek(f, SEEK_END, 0);
+    
+    size_t fSize = ftell(f);
+    fseek(f, SEEK_SET, 0);
+    char *buf = malloc(fSize);
+    fread(buf, fSize, 1, f);
+    fclose(f);
     
     plist_t shshplist;
     
-    if (memcmp(opl, "bplist00", 8) == 0)
-        plist_from_bin(opl, shshsize, &shshplist);
+    
+    if (memcmp(buf, "bplist00", 8) == 0)
+        plist_from_bin(buf, (uint32_t)fSize, &shshplist);
     else
-        plist_from_xml(opl, shshsize, &shshplist);
+        plist_from_xml(buf, (uint32_t)fSize, &shshplist);
     
     
     plist_t ticket = plist_dict_get_item(shshplist, "ApImg4Ticket");
     
-    char *im4m;
-    size_t im4msize=0;
+    char *im4m = 0;
+    uint64_t im4msize=0;
     
     plist_get_data_val(ticket, &im4m, &im4msize);
     
-    plist_free(opl);
+    plist_free(shshplist);
     
+    return im4m;
 }
 
 static struct option longopts[] = {
@@ -47,6 +50,7 @@ static struct option longopts[] = {
     { "create-img4",     required_argument, NULL, 'c'},
     { "all-headers",     no_argument,        NULL, 'a'},
     { "im4m",           required_argument,        NULL, 'm'},
+    { "shsh",           required_argument,        NULL, 's'},
     { "im4p",           required_argument,        NULL, 'p'},
     { "im4r",           optional_argument,        NULL, 'r'},
     { NULL, 0, NULL, 0 }
@@ -62,6 +66,7 @@ void cmd_help(){
     printf("  -c, --create-img4 FILEPATH creates an img4 with the specified im4m, im4p and optionally an im4r(see options below)");
     printf("  -a, --all-headers         print all headers of all IM4Ps\n");
     printf("  -m, --im4m FILEPATH       Filepath for im4m (reading or writing, depending on Option)");
+    printf("  -s, --shsh FILEPATH       Filepath for shsh (for reading im4m)");
     printf("  -p, --im4p FILEPATH       Filepath for im4p (reading or writing, depending on Option)");
     printf("  -r, --im4r Nonce          Nonce for im4r (with hexadecimal encoding) reading or printing");
     printf("\n");
@@ -76,6 +81,7 @@ int main(int argc, const char * argv[]) {
     char *extractedFilePath = 0;
     char *createdImg4FilePath = 0;
     char *im4mFilePath = 0;
+    char *shshFilePath = 0;
     char *im4pFilePath = 0;
     char *im4rNonce = 0;
     
@@ -84,37 +90,6 @@ int main(int argc, const char * argv[]) {
     int create_flag = 0;
     int printNonceFlag = 0;
     int allHeaders_flag = 0;
-    
-    
-    /*{
-        FILE *f = fopen(argv[1], "r");
-        fseek(f, 0, SEEK_END);
-        size_t size = ftell(f);
-        fseek(f, 0, SEEK_SET);
-        
-        im4p = malloc(size);
-        fread(im4p, size, 1, f);
-        fclose(f);
-    }
-    {
-        FILE *f = fopen(argv[2], "r");
-        fseek(f, 0, SEEK_END);
-        size_t size = ftell(f);
-        fseek(f, 0, SEEK_SET);
-        
-        im4m = malloc(size);
-        fread(im4m, size, 1, f);
-        fclose(f);
-    }
-    {
-        size_t s= 0;
-        char * img4new = makeIMG4WithIM4PAndIM4M(im4p, im4m,&s);
-        FILE *f = fopen("img4new", "w");
-        fwrite(img4new, s, 1, f);
-        fclose(f);
-    }
-    
-    return 1;*/
     
     if (argc == 1){
         cmd_help();
@@ -139,6 +114,9 @@ int main(int argc, const char * argv[]) {
                 break;
             case 'm': // long option: "im4m"; can be called as short option
                 im4mFilePath = optarg;
+                break;
+            case 's': // long option: "shsh"; can be called as short option
+                shshFilePath = optarg;
                 break;
             case 'p': // long option: "im4p"; can be called as short option
                 im4pFilePath = optarg;
@@ -196,17 +174,19 @@ int main(int argc, const char * argv[]) {
         
         // Read im4p to buffer
         {
-        FILE *f = fopen(im4pFilePath, "r");
-        fseek(f, 0, SEEK_END);
-        size_t size = ftell(f);
-        fseek(f, 0, SEEK_SET);
-        
-        im4p = malloc(size);
-        fread(im4p, size, 1, f);
-        fclose(f);
+            FILE *f = fopen(im4pFilePath, "r");
+            fseek(f, 0, SEEK_END);
+            size_t size = ftell(f);
+            fseek(f, 0, SEEK_SET);
+            
+            im4p = malloc(size);
+            fread(im4p, size, 1, f);
+            fclose(f);
         }
         // Read im4m to buffer
-        {
+        if (shshFilePath) {
+            im4m = im4mFormShshFile(shshFilePath);
+        }else{
             FILE *f = fopen(im4mFilePath, "r");
             fseek(f, 0, SEEK_END);
             size_t size = ftell(f);
