e939309c7d629fb601008fbd6085216e6c993093
Nikias Bassen <nikias@gmx.li>
2016-12-28 17:08:59 +0100

    autogen.sh: Allow initializing source tree without configuring

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-
-aclocal
+gprefix=`which glibtoolize 2>&1 >/dev/null`
+if [ $? -eq 0 ]; then
+  glibtoolize --force
+else
+  libtoolize --force
+fi
+aclocal -I m4
 autoconf
 autoheader
 automake --add-missing
-./configure "$@"
+
+if [ -z $NOCONFIGURE ]; then
+    ./configure "$@"
+fi
 ./setBuildVersion.sh
