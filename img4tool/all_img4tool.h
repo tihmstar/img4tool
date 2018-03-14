e755d09a9d6c58a2aecce0ed2aba02c03e153c77
Viktor Oreshkin <imselfish@stek29.rocks>
2018-03-14 04:42:20 +0300

    autoconf is trash fire

--- img4tool/all_img4tool.h
+++ img4tool/all_img4tool.h
@@ -9,11 +9,13 @@
 #ifndef all_h
 #define all_h
 
+#include <config.h>
+
 #define error(a ...) printf("[Error] %s: ",__func__),printf(a)
 #define warning(a ...) printf("[Warning] %s: ",__func__),printf(a)
 
-#define VERSION_COMMIT_COUNT "93"
-#define VERSION_COMMIT_SHA "70b5778730cd3d32f9bf5b49de9e075141892f7f"
+#define VERSION_COMMIT_COUNT "103"
+#define VERSION_COMMIT_SHA "3c153b91a0d60c28810e5992df4ba01ae5c8b541"
 
 
 #endif /* all_h */
