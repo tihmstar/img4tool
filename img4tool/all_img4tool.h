a5e7027cad7d3848e4bfd9fff960c76e75d36dca
tihmstar <tihmstar@gmail.com>
2018-03-14 14:46:00 +0100

    merged #9

--- img4tool/all_img4tool.h
+++ img4tool/all_img4tool.h
@@ -15,6 +15,7 @@
 #ifdef DEBUG //this is for developing with Xcode
 #define IMG4TOOL_VERSION_COMMIT_COUNT "Debug"
 #define IMG4TOOL_VERSION_COMMIT_SHA "Build: " __DATE__ " " __TIME__
+#define HAVE_LIBCOMPRESSION
 #else
 #include <config.h>
 #endif
